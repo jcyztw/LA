96127ce8f166c108f3e0ef9e2ee1bf5ed0a04c18
Reack <gon5483@gmail.com>
2018-05-29 02:51:31 +0800

    Push the project. Hidden action bar.

---
+++ LineLA/app/src/main/java/com/example/uscclab/line_la/StartInterfaceActivity.java
@@ -0,0 +1,26 @@
+package com.example.uscclab.line_la;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartInterfaceActivity extends AppCompatActivity {
+
+//    public static boolean LoginOrNot = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_interface);
+
+        new android.os.Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent loginIntent = new Intent(StartInterfaceActivity.this, MainActivity.class);
+                StartInterfaceActivity.this.startActivity(loginIntent);
+                StartInterfaceActivity.this.finish();
+            }
+        }, 1500);
+    }
+}
