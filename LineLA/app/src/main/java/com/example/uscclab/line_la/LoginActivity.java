bf766b30dc61496712be0f057171f41b8ebdf569
Alger <gino63o7gmail.com>
2018-05-29 10:26:56 +0800

    Add barcode scanner.

--- LineLA/app/src/main/java/com/example/uscclab/line_la/LoginActivity.java
+++ LineLA/app/src/main/java/com/example/uscclab/line_la/LoginActivity.java
@@ -1,27 +1,39 @@
 package com.example.uscclab.line_la;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class LoginActivity extends AppCompatActivity {
 
-//    private static Barcode barcode;
+    private final static int CAMERA_RESULT = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
 
         Button btn_logIn = findViewById(R.id.btn_login);
         btn_logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 //                barcode.callCamera();
-                Intent Intent = new Intent(LoginActivity.this, MainActivity.class);
-                LoginActivity.this.startActivity(Intent);
-                LoginActivity.this.finish();
+                callCamera();
+
             }
         });
 
@@ -35,5 +47,89 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(homeIntent);
             }
         });
+    }
+
+    public void callCamera(){
+        String[] permissionNeed = {
+                Manifest.permission.CAMERA,
+        };
+        if( hasPermission(permissionNeed)){
+            Scanner();
+        }else {
+            getPermission();
+        }
+    }
+    private boolean hasPermission(String[] permission) {
+        if (canMakeSmores()) {
+            for (String permissions : permission) {
+                return (ContextCompat.checkSelfPermission(this, permissions) == PackageManager.PERMISSION_GRANTED);
+            }
+        }
+        return true;
+    }
+    public void Scanner(){
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.setPrompt("請對準條碼");
+        integrator.setCameraId(0);
+        integrator.setBeepEnabled(false);
+        integrator.setBarcodeImageEnabled(false);
+        integrator.setCaptureActivity(AnyOrientationCaptureActivity.class);
+        integrator.setOrientationLocked(false);
+        integrator.initiateScan();
+    }
+    @TargetApi(23)
+    public void getPermission(){
+        if(Build.VERSION.SDK_INT>=23) {
+            String[] permissionNeed = {
+                    Manifest.permission.CAMERA,
+            };
+            if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                Toast.makeText(this, "需要相機權限掃描條碼", Toast.LENGTH_SHORT).show();
+            }
+            requestPermissions(permissionNeed, CAMERA_RESULT);
+        }
+    }
+    private boolean canMakeSmores() {
+        return(Build.VERSION.SDK_INT> Build.VERSION_CODES.LOLLIPOP_MR1);
+    }
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int [] grantResults ){
+        switch (requestCode){
+            case CAMERA_RESULT:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Scanner();
+                } else {
+                    Toast.makeText(this,"需要相機權限掃描條碼",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }}
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if(result != null) {
+            if(result.getContents() == null) {
+                Log.d("####", "Cancelled scan");
+                this.finish();
+            } else {
+                Log.d("####", "Scanned");
+                Toast.makeText(this, "掃描結果: " + result.getContents(), Toast.LENGTH_LONG ).show();   // 顯示條碼
+
+                if (this.getIntent().getDataString() != null) {
+                    //this.getIntent().getDataString() : usccbarcodescanner://?callurl=http://mmm.lifeacademy.org/erpweb/testbarcodeapp&returnurl=http://mmm.lifeacademy.org/erpweb/Scancode/PutScanCode?username=
+
+                    //sendBarcode_returnWebPage(result.getContents());
+                    //this.finish();
+                }
+                else {
+                    Toast.makeText(this, "請從網頁開啟本程式", Toast.LENGTH_LONG).show();
+                    this.finish();
+                }
+            }
+        } else {
+            // This is important, otherwise the result will not be passed to the fragment
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
