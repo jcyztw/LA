632cbdb0a6de56907ced6add19cdfa971d53100a
Reack <gon5483@gmail.com>
2018-06-02 18:34:21 +0800

    finish activity_confirm_login.xml layout.

--- LineLA/app/src/main/java/com/example/uscclab/line_la/ConfirmLoginActivity.java
+++ LineLA/app/src/main/java/com/example/uscclab/line_la/ConfirmLoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.uscclab.line_la;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.BitmapShader;
@@ -15,6 +16,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -28,55 +31,63 @@ public class ConfirmLoginActivity extends AppCompatActivity {
         ImageView Img1 = (ImageView) findViewById(R.id.img_profile);
         // 取得螢幕解析度
         DisplayMetrics dm = new DisplayMetrics();
-        this.getWindowManager().getDefaultDisplay().getMetrics(dm);
-//        Img1.setImageResource(R.drawable.welcome);
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
         Img1.setImageBitmap(getRoundedCornerBitmap(
                 BitmapFactory.decodeResource(
                         getResources(), R.drawable.welcome),dm.widthPixels/2.0f,dm));
+
+        Button btn_confirm = findViewById(R.id.btn_confirm);
+        btn_confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goMain = new Intent(ConfirmLoginActivity.this, MainActivity.class);
+                startActivity(goMain);
+                finish();
+            }
+        });
+
+        Button btn_cancel = findViewById(R.id.btn_cancel);
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goLogin = new Intent(ConfirmLoginActivity.this, LoginActivity.class);
+                startActivity(goLogin);
+                finish();
+            }
+        });
     }
 
     public static Bitmap getRoundedCornerBitmap(Bitmap bitmap, float roundPx,DisplayMetrics dm)
     {
-        final int size = dm.widthPixels;
-        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
-                bitmap.getHeight(),
-                Bitmap.Config.ARGB_8888);
+        final int size = dm.widthPixels/4*3;
+
+        //縮小圖片
+        int mRadius = Math.min(dm.widthPixels, dm.heightPixels)/2;
+        float mScale = (mRadius * 2.0f) / Math.min(bitmap.getHeight(), bitmap.getWidth());
+        Matrix matrix = new Matrix();
+        matrix.postScale(mScale,mScale); //長寬比例
+
+        //取得縮小圖片
+        Bitmap bitmap1 = Bitmap.createBitmap(bitmap,0,
+                0,bitmap.getWidth(),bitmap.getHeight(),matrix,true);
+
+        //新增畫布
+        Bitmap output = Bitmap.createBitmap(size,size,Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(output);
         final int color = 0xff424242;
         final Paint paint = new Paint();
-        Rect rect = new Rect(size/4, 0, size+size/4,size);
-        RectF rectF = new RectF(rect);
 
+        // 在畫布上繪製圓角後的矩形(圓形)
+        Rect rect = new Rect(0, 0, size,size);
+        RectF rectF = new RectF(rect);
         paint.setAntiAlias(true);
         canvas.drawARGB(0, 0, 0, 0);
         paint.setColor(color);
         canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
-/*
-        BitmapShader bitmapShader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
-        int mRadius = Math.min(dm.widthPixels, dm.heightPixels)/2;
-        float mScale = (mRadius * 2.0f) / Math.min(bitmap.getHeight(), bitmap.getWidth());
-        Matrix matrix = new Matrix();
-        matrix.setScale(mScale, mScale);
-        bitmapShader.setLocalMatrix(matrix);
-        paint.setShader(bitmapShader);
-*/
 
-
-
+        //去掉圓角後矩形之外多餘的像素並繪製
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        canvas.drawBitmap(bitmap, rect, rect, paint);
-//        canvas.drawCircle(mRadius, mRadius, mRadius, paint);
+        canvas.drawBitmap(bitmap1, 0, 0, paint);
         return output;
-
-
-//        Paint mPaint = new Paint();
-//        Bitmap bitmap = drawableToBitmap(getDrawable());
-
-        //初始化BitmapShader，传入bitmap对象
-
-
-
-//        mPaint.setShader(bitmapShader);
-
     }
 }
